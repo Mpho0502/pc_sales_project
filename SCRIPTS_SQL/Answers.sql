4d84856f7e2bec01bacde0f3907f5ba53207ce09
Mpho <matebesimoe89@gmail.com>
2026-03-04 16:10:17 +0200

    Answers for No. 1, 2, 3 & 4

--- SCRIPTS_SQL/Answers.sql
+++ SCRIPTS_SQL/Answers.sql
@@ -3,14 +3,80 @@
 -- SQL PROJECT ANSWERS
 -- Dataset: PC Sales
 -- ======================================================
+--Create a database
+
+ CREATE DATABASE PC_SALES
+ GO;
+
+ --Load the table with the dataset
+
+ SELECT [Continent]
+      ,[Country_or_State]
+      ,[Province_or_City]
+      ,[Shop_Name]
+      ,[Shop_Age]
+      ,[PC_Make]
+      ,[PC_Model]
+      ,[Storage_Type]
+      ,[Customer_Name]
+      ,[Customer_Surname]
+      ,[Customer_Contact_Number]
+      ,[Customer_Email_Address]
+      ,[Sales_Person_Name]
+      ,[Sales_Person_Department]
+      ,[Cost_Price]
+      ,[Sale_Price]
+      ,[Payment_Method]
+      ,[Discount_Amount]
+      ,[Purchase_Date]
+      ,[Ship_Date]
+      ,[Finance_Amount]
+      ,[RAM]
+      ,[Credit_Score]
+      ,[Channel]
+      ,[Priority]
+      ,[Cost_of_Repairs]
+      ,[Total_Sales_per_Employee]
+      ,[PC_Market_Price]
+      ,[Storage_Capacity]
+  FROM [PC_SALES].[dbo].[pc_data];
+
+  SELECT * FROM [PC_SALES].[dbo].[pc_data];
+
 
 -- BASIC QUESTIONS
 
 -- 1. Count the total number of sales records.
+
+SELECT 
+COUNT (*) AS Total_number_of_sales_records
+FROM [PC_SALES].[dbo].[pc_data];
+
 -- 2. Count the number of sales per Continent.
+
+SELECT Continent,
+COUNT (*) AS Sales_per_Continent
+FROM [PC_SALES].[dbo].[pc_data]
+GROUP BY Continent
+ORDER BY Sales_per_Continent;
+
 -- 3. Count the number of sales per Country or State.
+
+SELECT Country_or_State,
+COUNT (*) AS Sales_per_Country_or_State
+FROM [PC_SALES].[dbo].[pc_data]
+GROUP BY Country_or_State
+ORDER BY Sales_per_Country_or_State;
+
 -- 4. List all distinct Shop Name values.
+
+SELECT DISTINCT Shop_Name 
+FROM [PC_SALES].[dbo].[pc_data];
+
 -- 5. Find the average Sale Price.
+
+
+
 -- 6. Find the highest and lowest Sale Price.
 -- 7. Count the number of sales by Payment Method.
 -- 8. Count the number of sales by Channel (Online vs Offline).
